2b3bc4be7ca8335ffd9ae14b802cb67b22f356b8
Ruth Torres <ruths.roy@gmail.com>
2021-10-07 14:10:01 -0700

    added user and post routes

---
+++ routes/api/post-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const {
+  getAllPosts,
+  getPostById,
+  addPost,
+  updatePost,
+  deletePost,
+  addReaction,
+  deleteReaction,
+} = require("../../controllers/thought-controller.js ");
+
+router.route("/").get(getAllPosts);
+
+router.route("/:userId").post(addPost);
+
+router
+  .route("/:userId/:postId")
+  .get(getPostById)
+  .put(updatePost)
+  .delete(deletePost);
+
+module.exports = router;
