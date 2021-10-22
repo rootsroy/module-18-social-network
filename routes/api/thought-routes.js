b30b1f56bf929b16d15aa9e3437934b2e8b0159a
Ruth Torres <ruths.roy@gmail.com>
2021-10-21 22:25:35 -0700

    fix routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -8,15 +8,16 @@ const {
   addReaction,
   deleteReaction,
 } = require("../../controllers/thought-controller");
-
+//get all posts
 router.route("/").get(getAllPosts);
 
+///create post
 router.route("/:userId").post(addPost);
 
 router
-  .route("/:userId/:postId")
-  .get(getPostById)
-  .put(updatePost)
-  .delete(deletePost);
+  .route("/:userId/:postId") //1st userID and 2nd post Id
+  .get(getPostById) //get one post
+  .put(updatePost) //update post
+  .delete(deletePost); //delete post
 
 module.exports = router;
